d9103f4f7b366ebd7f9b125ce2fca8aea7dad258
MustangYM <ymoomy@yeah.net>
2019-07-03 16:36:24 +0800

    初始化

---
+++ YMRERDylib/YMRERDylib/Main/YMRERDylib.h
@@ -0,0 +1,14 @@
+//
+//  YMRERDylib.h
+//  YMRERDylib
+//
+//  Created by MustangYM on 2019/7/3.
+//  Copyright © 2019 MustangYM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YMRERDylib : NSObject
+
+@end
+
---
+++ YMRERDylib/YMRERDylib/Main/YMRERDylib.mm
@@ -0,0 +1,13 @@
+//
+//  YMRERDylib.m
+//  YMRERDylib
+//
+//  Created by MustangYM on 2019/7/3.
+//  Copyright © 2019 MustangYM. All rights reserved.
+//
+
+#import "YMRERDylib.h"
+
+@implementation YMRERDylib
+
+@end
